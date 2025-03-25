24a87e349006710837ac4d1fd18e789bbf9dded6
José Eduardo <dev.souzaedu@gmail.com>
2025-03-25 20:26:13 -0300

    ol

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ZMfWJwgIMwHUt3ZOGjKwn4R3k9CBQUGebI0xxZbiUK8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
